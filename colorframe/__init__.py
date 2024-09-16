f75a1aa1820e8ca5511e07721f42b73ac6670223
Felix Soubelet <felix.soubelet@protonmail.com>
2024-09-16 16:15:58 +0200

    patch version

--- colorframe/__init__.py
+++ colorframe/__init__.py
@@ -9,7 +9,7 @@ from .__main__ import app  # noqa: F401  # imported for script creation at packa
 __title__ = "colorframe"
 __description__ = "A simple package to add colored borders on pictures."
 __url__ = "https://github.com/fsoubelet/ColorFrame"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Felix Soubelet"
 __author_email__ = "felix.soubelet@cern.ch"
 __license__ = "MIT"
